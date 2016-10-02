49b87c6fc135b0dbb2db876d0d23bbc4a6589f57
umatoma <umatomakun@gmail.com>
2016-10-02 10:58:37 +0900

    show sidebar

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,10 +2,12 @@ import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import ChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
 import { blueGrey900 } from 'material-ui/styles/colors';
 
 const styleFooter = {
@@ -18,26 +20,33 @@ const styleFooter = {
 const App = ({ children }) => (
   <MuiThemeProvider>
     <div>
-      <AppBar
-        title="Material React Todos"
-        iconElementRight={
-          <IconMenu
-            iconButtonElement={
-              <IconButton><MoreVertIcon /></IconButton>
-            }
-            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-          >
-            <MenuItem primaryText="Top" onTouchTap={() => { browserHistory.push('/'); }} />
-            <MenuItem primaryText="About" onTouchTap={() => { browserHistory.push('/about'); }} />
-            <MenuItem primaryText="Help" onTouchTap={() => { browserHistory.push('/help'); }} />
-          </IconMenu>
-        }
-      />
-      <div>{children}</div>
-      <footer style={styleFooter}>
-        <span>Copyright (C) 2016 umatoma. All Rights Reserved.</span>
-      </footer>
+      <div style={{ marginLeft: '256px' }}>
+        <AppBar
+          title="Material React Todos"
+          iconElementRight={
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+            >
+              <MenuItem primaryText="Top" onTouchTap={() => { browserHistory.push('/'); }} />
+              <MenuItem primaryText="About" onTouchTap={() => { browserHistory.push('/about'); }} />
+              <MenuItem primaryText="Help" onTouchTap={() => { browserHistory.push('/help'); }} />
+            </IconMenu>
+          }
+        />
+        <div>{children}</div>
+        <footer style={styleFooter}>
+          <span>Copyright (C) 2016 umatoma. All Rights Reserved.</span>
+        </footer>
+      </div>
+      <Drawer open>
+        <MenuItem primaryText="Top" leftIcon={<ChevronRight />} onTouchTap={() => { browserHistory.push('/'); }} />
+        <MenuItem primaryText="About" leftIcon={<ChevronRight />} onTouchTap={() => { browserHistory.push('/about'); }} />
+        <MenuItem primaryText="Help" leftIcon={<ChevronRight />} onTouchTap={() => { browserHistory.push('/help'); }} />
+      </Drawer>
     </div>
   </MuiThemeProvider>
 );
